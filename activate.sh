68ec4f9e60c91143046d0d47be9c0f5e3a68796f
Ace167 <117175793+Ace167@users.noreply.github.com>
2023-04-20 14:25:54 +0200

    Initial commit

---
+++ activate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source [/path/to/freqtrade folder/].env/bin/activate
+cd [/path/to/freqtrade folder]
+freqtrade trade --strategy [StrategyName] -c config.json
+
+# Remove [ and ] when done modifying
+
